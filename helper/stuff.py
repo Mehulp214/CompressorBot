00a7f68504e7e2afe173b2946ebe50f4767cd97d
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-03-29 10:09:59 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="github.com/"),
+                Button.url("DEVELOPER", url="t.me/Patil_Mehul2"),
             ],
         ],
     )
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="github.com/"),
+                Button.url("DEVELOPER", url="t.me/Patil_Mehul"),
             ],
         ],
     )
