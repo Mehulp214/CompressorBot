66852932afcab23528cccfd70e007aee895ec447
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-01 23:05:22 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -15,13 +15,26 @@
 from . import *
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=1322549723, cast=int)
-    LOG = config("LOG_CHANNEL", cast=int)
+    APP_ID = 13216322
+    API_HASH = "15e5e632a8a0e52251ac8c3ccbe462c7"
+    BOT_TOKEN = "7160872230:AAHmmk7eDW2FwtVkRLwl3dwzJALR-qiS15Q"
+    OWNER = 5642570692
+    LOG = -1001817380537
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
     LOGS.info(str(e))
     exit(1)
+
+#try:
+    # APP_ID = config("APP_ID", cast=int)
+    # API_HASH = config("API_HASH")
+    # BOT_TOKEN = config("BOT_TOKEN")
+    # OWNER = config("OWNER_ID", default=1322549723, cast=int)
+    # LOG = config("LOG_CHANNEL", cast=int)
+#except Exception as e:
+    # LOGS.info("Environment vars Missing")
+    # LOGS.info("something went wrong")
+    # LOGS.info(str(e))
+    #exit(1)
+    
